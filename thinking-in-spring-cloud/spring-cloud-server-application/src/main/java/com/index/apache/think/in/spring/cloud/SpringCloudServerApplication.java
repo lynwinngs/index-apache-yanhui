840a439c8a61cc213c22cd42a7a572f9864d2078
Apache <918648761@qq.com>
2020-04-21 08:28:54 +0800

    spring cloud init

---
+++ thinking-in-spring-cloud/spring-cloud-server-application/src/main/java/com/index/apache/think/in/spring/cloud/SpringCloudServerApplication.java
@@ -0,0 +1,19 @@
+package com.index.apache.think.in.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: SpringCloudServerApplication
+ * @Description: spring cloud 服务端应用
+ * @Author: Xiao Xuezhi
+ * @Date: 2020/4/20 22:56
+ * @Version： 1.0
+ */
+@SpringBootApplication
+public class SpringCloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServerApplication.class, args);
+    }
+}
