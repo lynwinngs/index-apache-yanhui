f22b60e989713a154c695b945d50e449b93b47cf
肖学智 <xiaoxuezhi@tjmeiteng.com>
2020-04-28 15:22:01 +0800

    @LoadBalanced 源码分析概括

--- thinking-in-spring-cloud/spring-cloud-client-consumer-application/src/main/java/com/index/apache/thinking/in/spring/cloud/controller/UserController.java
+++ thinking-in-spring-cloud/spring-cloud-client-consumer-application/src/main/java/com/index/apache/thinking/in/spring/cloud/controller/UserController.java
@@ -4,7 +4,6 @@ import com.index.apache.thinking.in.spring.cloud.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +18,13 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/user")
 public class UserController {
 
+    /**
+     * 使用微服务的服务名做 host 时，必须使用 @LoadBalanced 注解，否则会报错 java.net.UnknownHostException,
+     * 因为使用了 @LoadBalanced 的 RestTemplate 会被注入到 org.springframework.cloud.client.loadbalancer.LoadBalancerAutoConfiguration
+     * RestTemplate 会被放入 org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor 实例
+     * 在请求的时候，根据 RestTemplate 是否有拦截器来对 ClientHTTPRequest 进行不同的处理，
+     * 没有经过拦截器处理，无法识别以微服务的服务名做 host 的请求
+     */
     @Autowired
     @LoadBalanced
     private RestTemplate restTemplate;
