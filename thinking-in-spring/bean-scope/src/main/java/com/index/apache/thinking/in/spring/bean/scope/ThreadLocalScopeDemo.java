bb8a3b330a60cb6a5c037797c6ab5c30e537f787
Apache <918648761@qq.com>
2020-05-13 23:11:41 +0800

    add thread local scope demo

--- thinking-in-spring/bean-scope/src/main/java/com/index/apache/thinking/in/spring/bean/scope/ThreadLocalScopeDemo.java
+++ thinking-in-spring/bean-scope/src/main/java/com/index/apache/thinking/in/spring/bean/scope/ThreadLocalScopeDemo.java
@@ -1,13 +1,12 @@
 package com.index.apache.thinking.in.spring.bean.scope;
 
 import com.index.apache.thinking.in.spring.beans.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 
 /**
- * TODO
+ * ThreadLocal scope demo
  *
  * @Author xiaoxuezhi
  * @DATE 2020/5/13 17:41
@@ -15,19 +14,42 @@ import org.springframework.context.annotation.Scope;
  **/
 public class ThreadLocalScopeDemo {
 
-    @Autowired
+    @Bean
     @Scope(ThreadLocalScope.THREAD_LOCAL_SCOPE)
-    private User theadLocalUser;
+    public User theadLocalUser() {
+        return new User(System.nanoTime(), "theadLocalUser");
+    }
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.register(ThreadLocalScopeDemo.class);
 
+        applicationContext.addBeanFactoryPostProcessor(beanFactory -> {
+            beanFactory.registerScope(ThreadLocalScope.THREAD_LOCAL_SCOPE, new ThreadLocalScope());
+        });
+
         applicationContext.refresh();
 
-
+        getBeanByLookup(applicationContext);
 
         applicationContext.close();
+    }
+
+    private static void getBeanByLookup(AnnotationConfigApplicationContext applicationContext) {
+        for (int i = 0; i < 3; i++) {
+            Thread thread = new Thread(()->{
+                for (int j = 0; j < 3; j++) {
+                    User user = applicationContext.getBean(User.class);
+                    System.out.printf("thread [%d] user:%s \n", Thread.currentThread().getId(), user);
+                }
+            });
+            thread.start();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
                                                   
